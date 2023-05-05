ad527e314de7972f9b296e679aa63ae64ba9bb84
RUIRUIashui <1650227792@qq.com>
2023-05-05 15:16:11 -0400

    add details， create NFT, and transfer NFT

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -8,6 +8,15 @@ contract NFTCollection is ERC721, ERC721Enumerable {
   mapping(string => bool) _tokenURIExists;
   mapping(uint => string) _tokenIdToTokenURI;
   mapping(uint => tokenDetails) public _tokenIdToDetail;
+  
+  struct tokenDetails{
+    uint tokenId;
+    string name;
+    string description;
+  }
+
+  event NewNFTCollectionMint(uint id, string tokenURI, string name, string description);
+  event NFTTransferedByOwner(address from, address to, uint id);
 
   constructor() 
     ERC721("NFT Collection", "NFT") 
@@ -25,5 +34,22 @@ contract NFTCollection is ERC721, ERC721Enumerable {
   function tokenURI(uint256 tokenId) public override view returns (string memory) {
     require(_exists(tokenId), 'ERC721Metadata: URI query for nonexistent token');
     return _tokenIdToTokenURI[tokenId];
+  }
+
+  function createNFT(string memory _tokenURI, string memory _name, string memory _description) public {
+    require(!_tokenURIExists[_tokenURI], 'The token URI should be unique');
+    tokenURIs.push(_tokenURI);    
+    uint _id = tokenURIs.length;
+    _tokenIdToTokenURI[_id] = _tokenURI;
+    _safeMint(msg.sender, _id);
+    _tokenURIExists[_tokenURI] = true;
+    _tokenIdToDetail[_id] = tokenDetails(_id,_name,_description);
+    emit NewNFTCollectionMint(_id, _tokenURI,_name, _description);
+  }
+
+  function transferNFT(uint _id, address _to) public {
+    require(msg.sender == super.ownerOf(_id), 'Only the owner can transfer the NFT.');
+    emit NFTTransferedByOwner(msg.sender, _to, _id);
+    return super.safeTransferFrom(msg.sender, _to, _id);
   }
 }
