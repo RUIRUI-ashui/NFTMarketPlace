26e2f4d0abe2a8d25b7fa3fc615ab94c0c058cba
RUIRUIashui <1650227792@qq.com>
2023-05-05 17:43:10 -0400

    added structure for purchase and listforpurchase function

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -7,10 +7,38 @@ contract NFTMarketplace {
   mapping (uint => _Purchase) public purchases;
   mapping (address => uint) public userFunds;
   NFTCollection nftCollection;
-  
+
+  struct _Purchase {
+    uint purchaseId;
+    uint id;
+    address user;
+    uint price;
+    bool purchased;
+    bool removed;
+  }
+
+  event Purchase(
+    uint purchaseId,
+    uint id,
+    address user,
+    uint price,
+    bool purchased,
+    bool removed
+  );
+
+  event PurchaseFilled(uint purchaseId, uint id, address newOwner);
+  event PurchaseCancelled(uint purchaseId, uint id, address owner);
+  event ClaimFunds(address user, uint amount);
 
   constructor(address _nftCollection) {
     nftCollection = NFTCollection(_nftCollection);
+  }
+  
+    function listNFTForSale(uint _id, uint _price) public {
+    nftCollection.transferFrom(msg.sender, address(this), _id);
+    purchaseCount ++;
+    purchases[purchaseCount] = _Purchase(purchaseCount, _id, msg.sender, _price, false, false);
+    emit Purchase(purchaseCount, _id, msg.sender, _price, false, false);
   }
 
 }
