e781837c796404b9278355f4794c123d7cbd7382
yhz16 <hy2762@columbia.edu>
2023-05-05 17:57:17 -0400

    purchase and fallback implementation

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -41,12 +41,24 @@ contract NFTMarketplace {
     emit Purchase(purchaseCount, _id, msg.sender, _price, false, false);
   }
 
-  function purchaseNFT(uint _purchaseId) public payable {}
+  function purchaseNFT(uint _purchaseId) public payable {
+    _Purchase storage _purchase = purchases[_purchaseId];
+    require(_purchase.purchaseId == _purchaseId, 'The purchase must exist');
+    require(_purchase.user != msg.sender, 'The owner of the purchase cannot fill it');
+    require(!_purchase.purchased, 'An purchase cannot be purchased twice');
+    require(!_purchase.removed, 'A removed purchase cannot be purchased');
+    require(msg.value == _purchase.price, 'The ETH amount should match with the NFT Price');
+    nftCollection.transferFrom(address(this), msg.sender, _purchase.id);
+    _purchase.purchased = true;
+    userFunds[_purchase.user] += msg.value;
+    emit PurchaseFilled(_purchaseId, _purchase.id, msg.sender);
+  }
 
   function removeNFTFromSale(uint _purchaseId) public {}
 
   function claimFunds() public {}
 
-  fallback () external {}
-
+  fallback () external {
+    revert();
+  }
 }
